ab2b748fccb86b857a692381414becaa0abc204c
Ken Sonoda <ksoda@users.noreply.github.com>
2020-07-04 19:23:11 +0900

    make non-GET requests from hyperlinks

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -1,9 +1,11 @@
 import "../styles/application.scss";
+import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 
 require("alpinejs");
 
 export function run() {
+  Rails.start();
   Turbolinks.start();
   console.log("Hello World from Webpacker");
 }
